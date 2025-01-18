f067cbd2ee33f2430caac9a5f1ed91060a12e8e9
SupunLiyanage88 <liyanagesupun10@gmail.com>
2025-01-18 22:20:18 +0530

    fix

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,12 +4,6 @@ import ShinyText from './Tools/ShinyText.jsx';
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleContactClick = (e) => {
-    e.preventDefault();
-    window.location.href = '/contact';
-  };
-
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -38,8 +32,7 @@ function Navbar() {
       </nav>
 
       <a
-        href="#"
-        onClick={handleContactClick} // Call the function when clicked
+        href="/contact"
         className="relative py-2 px-8 text-white text-base font-bold rounded-full overflow-hidden bg-blue-500 transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
         style={{ textDecoration: 'none' }}
       >
